10ee90ba326714905787f082b858742a16410d37
Mule <3.7mules@gmail.com>
2016-09-24 15:38:34 -0500

    Adding skeleton, interrupted mid-tinkering

---
+++ main.py
@@ -0,0 +1,12 @@
+# File main.py
+# Purpose: Program should be run from this file
+# Goal: keep this file as clean as possible
+#       import what we need from other files
+import game_funcs
+
+
+print("Welcome to Blackjack!\n")
+game_funcs.play_game()
+print("Thanks for playing!  Goodbye!\n")
+
+
