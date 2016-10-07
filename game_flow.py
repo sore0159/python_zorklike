168335b6cf2387e53260d1d96e60d18896610c79
Mule <3.7mules@gmail.com>
2016-10-07 17:23:40 -0500

    added get_int, fixed game_flow to use only io_user for in/out

--- game_flow.py
+++ game_flow.py
@@ -1,7 +1,9 @@
 # File game_flow.py
 # Purpose: hold all our junk, for now
 
-import io_user
+# io_user is overspecified in case we ever want io_filesystem or something
+# for now, though, we can pretend it's our only io to make things easier
+import io_user as io
 
 # the foundational game loop
 # if we want to track game win/losses, it needs to return a win/loss value
@@ -11,20 +13,24 @@ def play_game():
     game_score = 0
     valid_list = ["hit", "stand", "cheat"]
     while True:
-        io_user.display("Your current score is %d"% game_score)
-        command = io_user.get_command(valid_list)
+        io.display("Your current score is %d"% game_score)
+        command = io.get_command(valid_list)
         if command == "stand":
-            io_user.display("You stand at %d" % game_score)
+            io.display("You stand at %d" % game_score)
             return
         elif command == "cheat":
-            game_score=int(input("What would you like your score to be?"))
-            io_user.display("You've changed your score to %d"% game_score)
+            io.display("What value would you like your score to be?")
+            game_score = io.get_int()
+            io.display("You've changed your score to %d"% game_score)
+            if game_score > 21:
+                io.display("You lose!")
+                return
         else:
             val = 5 # todo randomize
-            io_user.display("You are dealt a %d"% val)
+            io.display("You are dealt a %d"% val)
             game_score += val 
             if game_score > 21:
-                io_user.display("You bust at %d" % game_score)
+                io.display("You bust at %d" % game_score)
                 return
 
 
--- io_user.py
+++ io_user.py
@@ -19,3 +19,16 @@ def get_command(valid_list):
         print("Invalid command!  Please try again!")
         print("Valid commands are: "+", ".join(valid_list))
 
+
+# possible additions: 
+#       a way for the user to cancel?  If so, all calls to this function must check and handle user cancels.  May be better to have a separate cancellable function.
+#       sometimes redisplaying the original prompt, which a user might forget after enough failed entries
+def get_int():
+    while True:
+        user_input = input("Value: ")
+        try:
+            val = int(user_input) # I still don't quite trust int(x) as a conversion tool, but it seems the way to go
+        except:
+            print("Please enter a valid number!")
+        else:
+            return val
